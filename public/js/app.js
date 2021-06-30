f9c2b701fe327c8601f6d3cc9c4d70e43f80e27a
duncan <duncankithinji91@gmail.com>
2021-06-30 14:52:55 +0300

    Config for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", (e) => {
   const location = search.value;
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch("http://localhost:3000/weather?address=" + location)
+  fetch("/weather?address=" + location)
     .then((response) => {
       return response.json();
     })
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express Config
 const staticPath = path.join(__dirname, "../public");
@@ -88,6 +89,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(port, () => {
+  console.log("Listening on port " + port);
 });
